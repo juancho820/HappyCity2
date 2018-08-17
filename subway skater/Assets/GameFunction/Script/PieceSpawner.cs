e348c0d1d18ca1607ef44ae4d16d26f943c35526
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-08-16 22:10:13 -0500

    bugs fixes

--- subway skater/Assets/GameFunction/Script/PieceSpawner.cs
+++ subway skater/Assets/GameFunction/Script/PieceSpawner.cs
@@ -146,7 +146,7 @@ public class PieceSpawner : MonoBehaviour
         i = transform.childCount;
         posicion = transform.GetChild(i - 1).gameObject.transform.position;
         transform.GetChild(i - 1).gameObject.transform.position = new Vector3(0, 0, 0);
-        yield return new WaitForSeconds(1.4f);
+        yield return new WaitForSeconds(1.8f);
         transform.GetChild(i - 1).gameObject.transform.position = posicion;
         //transform.GetChild(i-1).gameObject.SetActive(true);
     }
