807c0de50e20159e4d5d656e1139cb17491ee515
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-08-18 10:26:47 -0500

    data loader y data inserter

---
+++ subway skater/Assets/GameFunction/Script/DataInserter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataInserter : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateUser();
+        }  
+    }
+
+    public void CreateUser()
+    {
+        WWW itemsData = new WWW("http://localhost/Corre_Tin/InsertCodes.php");
+    }
+}
