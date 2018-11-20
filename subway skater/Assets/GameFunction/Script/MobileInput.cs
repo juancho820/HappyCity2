3d99836fc92454cd29eddff45700abca8ecb8594
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-11-20 17:36:59 -0500

    fixes

--- subway skater/Assets/GameFunction/Script/MobileInput.cs
+++ subway skater/Assets/GameFunction/Script/MobileInput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MobileInput : MonoBehaviour
 {
-    private const float DEADZONE = 50.0f;
+    private const float DEADZONE = 40.0f;
 
     public static MobileInput Instance { set; get; }
 
